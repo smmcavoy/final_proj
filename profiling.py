ad8aec2040fd7c40b063bb83a96cfe491826df3d
smmcavoy <mcavoyseamus@gmail.com>
2025-11-30 16:02:27 -0500

    initial commit

---
+++ profiling.py
@@ -0,0 +1,14 @@
+import ydata_profiling
+import pandas as pd
+import os
+
+data_dir = r'/home/mcavoyse/coursework/DAMG7370/final_proj/data'
+save_dir = r'/home/mcavoyse/coursework/DAMG7370/final_proj/profiles'
+
+os.makedirs(save_dir, exist_ok=True)
+
+for fname in os.listdir(data_dir):
+    print(f'Processing {fname}...')
+    savefilename = fname.replace('.csv', '_profile.html')
+    df = pd.read_csv(os.path.join(data_dir, fname), sep='\t')
+    ydata_profiling.ProfileReport(df).to_file(os.path.join(save_dir, savefilename))
