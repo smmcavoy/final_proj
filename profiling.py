68dff6c167db901442b8de534c75da498963a15d
smmcavoy <mcavoyseamus@gmail.com>
2025-12-02 19:54:56 -0500

    update profiles with \N interpreted as null

--- profiling.py
+++ profiling.py
@@ -10,5 +10,5 @@ os.makedirs(save_dir, exist_ok=True)
 for fname in os.listdir(data_dir):
     print(f'Processing {fname}...')
     savefilename = fname.replace('.tsv', '_profile.html')
-    df = pd.read_csv(os.path.join(data_dir, fname), sep='\t', nrows=1000000)
+    df = pd.read_csv(os.path.join(data_dir, fname), sep='\t', nrows=1000000, na_values=['\\N'])
     ydata_profiling.ProfileReport(df).to_file(os.path.join(save_dir, savefilename))
